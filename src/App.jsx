19f0ce64fc53d55d3c4b594af7b33cb9d5ff834c
leohb07 <leohb1501@gmail.com>
2022-09-12 23:24:27 -0300

    Criacao do projeto

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Header } from "./components/Header/Header";
+import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+
+import styles from "./App.module.css";
+
+import './global.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Leonardo Barrocal" 
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae sit officia eligendi in quas sunt obcaecati eveniet magnam, quisquam nam exercitationem quis ipsum beatae ab animi libero, dolores eius dicta."
+          />
+          <Post 
+            author="Leonardo Barrocal" 
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae sit officia eligendi in quas sunt obcaecati eveniet magnam, quisquam nam exercitationem quis ipsum beatae ab animi libero, dolores eius dicta."
+          />
+        </main>
+      </div>
+    </div>
+  );  
+}
+
